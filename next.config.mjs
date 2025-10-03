c72030e72cdebebafffbb6bb4d62c1c4682b2633
isaac seiler <isaaciseiler@gmail.com>
2025-10-03 16:56:34 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: false,
+    unoptimized: false, // let next/image optimize & serve responsive formats
     remotePatterns: [
-      { protocol: "https", hostname: "isaacs-portfolio.r2.dev" },
-      // if you later add a custom domain like media.example.com, add it here too:
-      // { protocol: "https", hostname: "media.example.com" }
+      { protocol: "https", hostname: "pub-41d52824b0bb4f44898c39e1c3c63cb8.r2.dev" }
+      // add your custom media domain here later if you make one
     ]
   },
   experimental: {
